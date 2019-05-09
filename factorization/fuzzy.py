6e36d0f590baf49f3776d2edc96188dcff0ca445
Arthur Guillon <art.guillon@gmail.com>
2019-05-09 14:58:46 +0200

    Refactoring bis

--- factorization/fuzzy.py
+++ factorization/fuzzy.py
@@ -6,21 +6,6 @@ import factorization.commons as commons
 from scipy.optimize import LinearConstraint, Bounds, minimize
 
 
-def pca_hull_initialization(X):
-    """Initialization algorithm from Bauckhage & Thurau (2009)"""
-    pca = PCA(n_components=0.95) # must explain 95% of the variance
-    X -= X.mean()
-    Xpca = pca.fit_transform(X)
-    indices = set()
-    for (i,j) in combinations(range(pca.n_components_),2):
-        Xproj = Xpca[:,(i,j)]
-        hull = ConvexHull(Xproj)
-        indices.update(hull.vertices)
-    return np.array(sorted(indices))
-
-def _least_squares_cost(X, centers, memberships):
-    return np.linalg.norm(X - memberships.dot(centers))**2
-
 # TODO: - implement accelerated version and other initialization methods
 #       - generalize projected gradient descent to enforce summation constraint?
 class FactorizedFCM(nmf.NMF):
