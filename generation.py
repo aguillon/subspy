5175d51c7692d6540377b3118a08b191cf8f4b16
Arthur Guillon <art.guillon@gmail.com>
2019-05-21 22:15:23 +0200

    Clean-up of generation.py, adding a function to generate time series

--- generation.py
+++ generation.py
@@ -1,15 +1,10 @@
 import numpy as np
 import misc
 
-# ls elements have should be immutable
+from numpy.random import randint
+
 def make_diagonal(ls):
-    r = []
-    n = len(ls)
-    for i in range(n):
-        l = [0 for j in range(n)]
-        l[i] = ls[i]
-        r.append(l)
-    return r
+    return np.diagflat(ls)
 
 def identity_mat(n, normalize=False):
     if normalize:
@@ -18,23 +13,33 @@ def identity_mat(n, normalize=False):
         return make_diagonal([1 for i in range(n)])
 
 # TODO? Take optional context object in order to ease up the
-# writing of scripts
-def gaussian(center, size, cov = None, seed = None):
+# writing of scripts (e.g. should we return the classes
+# or not)
+def gaussian(center, size, cov = None, seed = None, with_y = None):
     if seed:
         np.random.seed(seed)
     if not cov:
         cov = identity_mat(len(center))
-    elif type(cov) == list and type(cov[0]) != list:
+    if type(cov) == list:
+        flatten_me = False
+        if(type(cov[0])) == np.ndarray:
+            flatten_me = True
+        cov = np.array(cov)
+        if flatten_me:
+            cov = np.hstack(cov)
+    assert type(cov) == np.ndarray, "generation.gaussian now accepts arrays"
+    if len(cov.shape) == 1:
         cov = make_diagonal(cov)
-    elif type(cov) != list:
-        raise ValueError("cov", cov)
     t = np.random.multivariate_normal(center, cov, size)
-    return t, [0] * size
+    if with_y is None:
+        return t
+    else:
+        return t, np.full(size, with_y)
 
 def gaussians(centers, sizes, covs):
     r = []
     for i,c in enumerate(centers):
-        r.extend(gaussian(c, sizes[i], covs[i])[0])
+        r.extend(gaussian(c, sizes[i], covs[i]))
     colors = [[i] * sizes[i] for i in range(len(sizes))]
     colors = misc.flatten(colors)
     r = np.array(r)
@@ -47,3 +52,32 @@ def add_noise(points, r=0.1):
     noise = np.random.uniform(np.min(points, axis=0), np.max(points, axis=0),
             size = (size,d))
     return np.concatenate((points, noise))
+
+def time_series(n, d, n_clusters, l_segments=(3,6), offsets=None):
+    """Simple generator of real-valued time series, with some distinctive
+    portion for each cluster."""
+    if type(l_segments) == int:
+        l_segments = (l_segments, l_segments+1)
+    if offsets is None:
+        offsets = np.zeros((n_clusters, d))
+    X = []
+    y = []
+    for r in range(n_clusters):
+        if r < n_clusters - 1:
+            size = int(n/n_clusters)
+        else:
+            size = n - len(X)
+        pattern_length = randint(*l_segments)
+        pattern_start = randint(0, d-pattern_length)
+        # building the variance vector including the pattern
+        print(pattern_start)
+        variances = list()
+        variances.append(randint(10, 20, size=pattern_start)/10)
+        variances.append(randint(1, 5, size=pattern_length)/30)
+        variances.append(randint(10, 20, size=d-(pattern_length+pattern_start))/10)
+        X1, y1 = gaussian(offsets[r], size, variances, with_y = r)
+        X.extend(X1)
+        y.extend(y1)
+    return np.array(X), np.array(y)
+
+
